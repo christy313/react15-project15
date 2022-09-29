ffc37f261a761b8b22401e55972bb9788766e0b3
christy313 <christycreativity@gmail.com>
2022-09-29 13:53:43 -0400

    Change error page route

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
           <Route path="/cocktail/:id">
             <SingleCocktail />
           </Route>
-          <Route path="*">
+          <Route path="/*">
             <Error />
           </Route>
         </Switch>
