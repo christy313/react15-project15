f7f4362550c89aaeafb60c380343d1bfbef84c96
christy313 <christycreativity@gmail.com>
2022-09-29 13:32:08 -0400

    Change router path

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/react15-project15">
             <Home />
           </Route>
           <Route path="/about">
