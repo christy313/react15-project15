1efed58b0b049bfaad83036c6375e59a00f17dea
christy313 <christycreativity@gmail.com>
2022-09-29 13:10:52 -0400

    Updates

---
+++ precache-manifest.49012bbf88e80752d6c080b4b0f3ccc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa812c7ecca353f1d1f4f4d9d2b98e0",
+    "url": "/react15-project15/index.html"
+  },
+  {
+    "revision": "626f063b5e6c3d584523",
+    "url": "/react15-project15/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "dac2456f2d87e47115ee",
+    "url": "/react15-project15/static/js/2.48c2207b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react15-project15/static/js/2.48c2207b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626f063b5e6c3d584523",
+    "url": "/react15-project15/static/js/main.6fd52614.chunk.js"
+  },
+  {
+    "revision": "2506904bfea85d54c7a8",
+    "url": "/react15-project15/static/js/runtime-main.ff621fc8.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/react15-project15/static/media/logo.9a3d2645.svg"
+  }
+]);
